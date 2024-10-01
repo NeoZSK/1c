e086d96f9ab033036188fd304d9d5777fd24b5d4
Oskar Legner <oskarlegner@gmail.com>
2024-10-01 18:26:15 +0200

    Draw rectangle with for loop

---
+++ GameBoard-common/GameBoard-common.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int width = 10;
+	int height = 5;
+
+
+	for (int i = 0; i < height; i++) {
+		for (int i = 0; i < width; i++) {
+
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+
+
+
+
+
+}
