315baa876f850ad619a873c707fbbb3cf8f72f74
Oskar Legner <oskarlegner@gmail.com>
2024-10-02 12:46:21 +0200

    Draw a board and add coords

--- GameBoard/GameBoard-common.cpp
+++ GameBoard/GameBoard-common.cpp
@@ -4,21 +4,31 @@ using namespace std;
 
 int main()
 {
-	int width = 10;
-	int height = 5;
+	const char CHARACTER = '*';
 
+	int width = 31;
+	int height = 15;
+
+	// spawnpoint -> 0,0
+	int x, y;
+	x = 0;
+	y = 0;
+
+	cout << "Enter x coord: ";
+	cin >> x;
+	cout << "Enter y coord: ";
+	cin >> y;
 
 	for (int i = 0; i < height; i++) {
-		for (int i = 0; i < width; i++) {
-
-			cout << "*";
+		for (int j = 0; j < width; j++) {
+			if (x == j && y == i) {
+				cout << CHARACTER;
+			} else {
+				cout << ".";
+			}
 		}
 		cout << endl;
 	}
-
-
-
-
 
 
 }
