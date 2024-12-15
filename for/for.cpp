29fed08253ab79596e4c0d0711abb3024027ad72
Oskar <oskar.legner@zsk.poznan.pl>
2024-12-15 23:01:12 +0100

    Add for-loop showcase

---
+++ for/for.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int hours;
+	cout << "How long should it take? (h)";
+	cin >> hours;
+
+	for (int i = 1; i <= hours; i++) {
+		cout << "It's " << i << "hour" << endl;
+	}
+
+}
