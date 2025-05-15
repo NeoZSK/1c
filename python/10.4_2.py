4d5014840b5b44a5a063c9def153d2e6b04ba75a
Oskar <oskar.legner@zsk.poznan.pl>
2025-05-15 09:51:18 +0200

    add 10.4

--- python/10.4_2.py
+++ python/10.4_2.py
@@ -1,2 +1,21 @@
-k = input("What level you're exping to?")
+k = int(input("What level you're exping to?"))
 
+elem = None
+
+
+for i in range(1,k):
+    if k == 1:
+        elem = 1
+        
+    if k == 2:
+        prevElem = 1
+        elem = 3
+        
+    if k > 2:
+        
+        elem = elem + prevElem
+        # Przekazanie kolejnego prevElem
+        
+    
+
+print(elem)
