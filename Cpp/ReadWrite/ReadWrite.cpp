2bfa90a1f76dbe8d66c380200efcd90067d71cdc
Oskar <oskar.legner@zsk.poznan.pl>
2025-06-05 15:51:11 +0200

    Add ReadWrtie in cpp code

---
+++ Cpp/ReadWrite/ReadWrite.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    fstream file("./numbers.txt", ios::in);
+    fstream fileOut("./results.txt", ios::out);
+    string line;
+
+    int sum = 0;
+
+    if (!file.is_open()) {
+        cout << "error: cannot open file" << endl;
+        return 1;
+    }
+    
+
+    // Read line by line
+    while (getline(file, line)){
+        int newVal = stoi(line) - 100;
+        fileOut << newVal << endl;
+    }
+
+    file.close();
+    fileOut.close();
+}
---
+++ Cpp/ReadWrite_2/ReadWrite_2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // ctrl + R, ctrl + R -> change name for all
+    fstream inFile("./numbers.txt", ios::in);
+    fstream outFile("./results.txt", ios::out);
+    string line;
+    int sum = 0;
+
+    if (!inFile.is_open()) {
+        cout << "error: cannot open file" << endl;
+        return 1;
+    }
+
+    while (getline(inFile, line)) {
+        int newVal = stoi(line) - 100;
+        outFile << newVal << endl;
+    }
+
+    //cout << "sum is " << sum << endl;
+
+    inFile.close();
+    outFile.close();
+}
